652f09fb34db72aac4e605ffa7ae758ab3ca4c20
cmargieson <cmargieson@gmail.com>
2024-11-25 18:45:40 +1100

    content

---
+++ blog/2024-11-15-rydal/index.md
@@ -0,0 +1,81 @@
+---
+tags: [motorbike]
+---
+
+import Image from '@theme/IdealImage';
+
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+import MapComponent from '@site/src/components/MapComponent';
+import rydal from './rydal.kml'
+
+# Rydal
+
+<MapComponent
+    style={{ height: "650px", width: "100%" }}
+    title={"rydal"}
+    urls={[rydal]}
+/>
+
+<figure>
+<Image img={require('./sugarmans-road.jpg')} />
+<figcaption>Sugarmans Road</figcaption>
+</figure>
+
+<figure>
+<Image img={require('./coxs-river-crossing.jpg')} />
+<figcaption>Coxs River Crossing</figcaption>
+</figure>
+
+<figure>
+<Image img={require('./mount-walker-trig-station.jpg')} />
+<figcaption>Mount Walker Trig Station</figcaption>
+</figure>
+
+<figure>
+<Image img={require('./mount-walker-road-waiting-for-recovery-team.jpg')} />
+<figcaption>Mount Walker Road, waiting for recovery team</figcaption>
+</figure>
+
+<figure>
+<Image img={require('./mount-walker-road-recovery-team-arriving.jpg')} />
+<figcaption>Mount Walker Road, recovery team arriving</figcaption>
+</figure>
+
+<figure>
+<Image img={require('./mount-walker-road.jpg')} />
+<figcaption>Mount Walker Road</figcaption>
+</figure>
+
+<figure>
+<LiteYouTubeEmbed
+    id="QzTv0wZeIXM"
+    title="Rydal - Lake Lyle Road"
+    poster="maxresdefault"
+  />
+<figcaption>Lake Lyle Road</figcaption>
+</figure>
+
+<figure>
+<LiteYouTubeEmbed
+    id="NVVo8hE5wzw"
+    title="Rydal - Coxs River Crossing"
+    poster="maxresdefault"
+  />
+<figcaption>Coxs River Crossing</figcaption>
+</figure>
+
+<figure>
+<LiteYouTubeEmbed
+    id="PfooyXUk2JU"
+    title="Rydal - Gauging Station Road"
+    poster="maxresdefault"
+  />
+<figcaption>Gauging Station Road</figcaption>
+</figure>
+
+<figure>
+<Image img={require('./mount-victoria-station.jpg')} />
+<figcaption>Mount Victoria Station</figcaption>
+</figure>
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,9 +36,16 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      footer: {
-        style: "dark",
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      navbar: {
+        title: "Trip Notes",
+        items: [
+          {
+            // The name to be shown for this item.
+            label: "Motorbike Routes",
+            // Client-side routing, used for navigating within the website. The baseUrl will be automatically prepended to this value.
+            to: "/motorbike-routes",
+          },
+        ],
       },
       prism: {
         theme: prismThemes.github,
@@ -55,12 +62,12 @@ const config = {
         max: 1030, // max resized image's size.
         min: 640, // min resized image's size. if original is lower, use that size.
         steps: 2, // the max number of images generated between min and max (inclusive)
-        disableInDev: false,
+        // disableInDev: false,
       },
     ],
 
     // Custom Plugin
-    function myPlugin(context, opts) {
+    function myPlugin() {
       // ...
       return {
         // A compulsory field used as the namespace for directories to cache
@@ -68,9 +75,9 @@ const config = {
         // If you're writing your own local plugin, you will want it to
         // be unique in order not to potentially conflict with imported plugins.
         // A good way will be to add your own project name within.
-        name: "docusaurus-plugin",
+        name: "trip-notes-webpack-plugin",
 
-        configureWebpack(config, isServer, utils, content) {
+        configureWebpack() {
           // Modify internal webpack config. If returned value is an Object, it
           // will be merged into the final config using webpack-merge;
           // If the returned value is a function, it will receive the config as the 1st argument and an isServer flag as the 2nd argument.
